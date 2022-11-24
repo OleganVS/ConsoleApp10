cd53c6187557dcaf572844d56a3ac1411d8f54a5
OleganVS <118183808+OleganVS@users.noreply.github.com>
2022-11-24 12:22:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string str = Console.ReadLine();
+            string[] strarray= str.Split();
+            string max = strarray[2];
+            foreach(string s in strarray) 
+            {
+                if (s.Length > max.Length) 
+                {
+                    max = s; 
+                }
+                Console.WriteLine(max);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str1 = "";
+            foreach(char c in str) 
+            {
+                str1 = c + str1;
+            }
+            Console.WriteLine(str1);
+            Console.ReadKey();
+        }
+    }
+}
